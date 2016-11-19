1e34273667b9fe8a1c0ecdc467cd25af3dfa6de6
Rob Aben <robaben11@gmail.com>
2016-11-19 01:19:49 +0100

    little cleanup

--- public/libs/file-upload.js
+++ public/libs/file-upload.js
@@ -4,22 +4,17 @@
     $(document).ready(function () {
         var encryption = new Encryption();
 
-        // Trigger file input on button click
         $('#upload-pdf-file-button-js').on('click', function () {
             //TODO: implement
         });
 
-        // Trigger file input on button click
         $('#create-hash-button-js').on('click', function () {
             //TODO: implement
         });
 
-        // Trigger file input on button click
         $('#generate-keys-button-js').on('click', function () {
             var keyPair = encryption.generateKeyPair();
             alert("Generated keypair :) ");
         });
-
-        $( '' );
     });
 })();
