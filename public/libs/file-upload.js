bff0e7b9af3af4f20fff9f84091630e0e7b7a9e2
Rob Aben <robaben11@gmail.com>
2016-11-19 01:19:02 +0100

    click listeners for buttons cleared

--- public/libs/file-upload.js
+++ public/libs/file-upload.js
@@ -1,60 +1,25 @@
-(function(){
-    $( document ).ready(function(){
-        const {dialog} = require('electron').remote;
-        const fs = require('fs'); // Load the File System to execute our common tasks (CRUD)
+(function () {
+    let Encryption = require('Encryption');
 
-
-        var fileUploader = new FileUploader();
+    $(document).ready(function () {
+        var encryption = new Encryption();
 
         // Trigger file input on button click
-        $( '#upload-file-button' ).on('click', function () {
-            console.log("YOLO");
-            dialog.showOpenDialog({ filters: [
-                { name: 'PDF', extensions: ['pdf', 'PDF'] }
-            ]},function (fileNames) {
-                if (fileNames === undefined) return;
-                    var fileName = fileNames[0];
-                    fs.readFile(fileName, 'utf-8', function (err, data) {
-                        $( '#preview' ).text( data );
-                });
-            });
-        } );
+        $('#upload-pdf-file-button-js').on('click', function () {
+            //TODO: implement
+        });
 
-        // When user selects a file
-        $( '#upload-file' ).on( 'change', function () {
-            fileUploader.uploadFile( this );
-        })
+        // Trigger file input on button click
+        $('#create-hash-button-js').on('click', function () {
+            //TODO: implement
+        });
+
+        // Trigger file input on button click
+        $('#generate-keys-button-js').on('click', function () {
+            var keyPair = encryption.generateKeyPair();
+            alert("Generated keypair :) ");
+        });
+
+        $( '' );
     });
 })();
-
-/** This class handles file uploads **/
-var FileUploader = function () {
-
-
-    // Save the file to the hdd
-    function uploadFile( caller ){
-        var files = $(caller).get(0).files;
-
-        if (files.length > 0){
-            // One or more files selected, process the file upload
-
-            // create a FormData object which will be sent as the data payload in the
-            // AJAX request
-            var formData = new FormData();
-
-            // loop through all the selected files
-            for (var i = 0; i < files.length; i++) {
-                var file = files[i];
-
-                // add the files to formData object for the data payload
-                formData.append('uploads[]', file, file.name);
-            }
-        }
-    }
-
-    function checkIfFileExists(){
-        //TODO: implement
-        return false;
-    }
-
-};
