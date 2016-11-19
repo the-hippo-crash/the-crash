b17bf65493e5a9b00e27962e6d0dadb7e0b868bb
Dennis Broeks <dennis@MacBook-Pro-van-Dennis.local>
2016-11-19 05:00:54 +0100

    Implemented navigation

---
+++ public/libs/navigator.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function($) {
+
+    /**
+     * Add event handlers for navigation links
+     */
+    $(document).ready(function() {
+        $('[data-open-page]').on('click', function(e) {
+            let page = $(e.target).attr('data-open-page');
+            openPage(page);
+        });
+    });
+
+    /**
+     * The page to open
+     * @param {string} page The page to open
+     */
+    function openPage(page) {
+        $('[data-page]').hide();
+        $('[data-page="' + page + '"').show();
+    }
+
+})(jQuery);
