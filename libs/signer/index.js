fa43350293f5a779aafc0117b2d2909b5e058885
Indy Jones <misterfu2@gmail.com>
2016-11-19 14:34:34 +0100

    push just in case

--- libs/signer/index.js
+++ libs/signer/index.js
@@ -14,6 +14,7 @@ class FileSigner {
   /**
    * Generates a signature for a file.
    * @param fileName
+   * @param {String} privateKey Optional location of the private key
    * @param {function} done Callback
    * @returns
    */
